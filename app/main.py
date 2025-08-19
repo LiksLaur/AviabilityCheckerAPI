d394e6c282ee3cff58da776f966d6c084c5cc181
Liks Laur <LiksLaurenCC@yahoo.com>
2025-08-19 21:21:40 +0300

    Added availability check and extensions of error

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Dict, List
 import requests
+import re
 
 app = FastAPI()
 
@@ -18,13 +19,25 @@ class SRequestModel(BaseModel):
 
 @app.post("/check", response_model=SRequestModel)
 def checkAvailbility(links: StatusResponse):
-    print(links.listLinks)  
-    return{
-  "request": {
-    "additionalProp1": {
-      "available": True,
-      "code": 0,
-      "info": "string"
-    }
-  }
-}
+    result = {}
+    for link in links.listLinks:
+        try:
+            r = requests.get(link)
+
+            code = int(re.findall(r'\[(.*?)\]', str(r))[0])
+            available = True if code >=200 and code <= 309 else False
+            info = re.sub(r'\[.*?\]', '', str(r))[1:-1]
+
+            print(link, code, available, info)  
+            result[link] = SSiteStatus(available=available, code=code, info=info)
+        
+        except requests.exceptions.ConnectionError as err:
+            result[link] = SSiteStatus(available=False, code=503, info=f"Service Unavailable: {str(err)}")
+        except requests.exceptions.Timeout as err:
+            result[link] = SSiteStatus(available=False, code=408, info=f"Request Timeout: {str(err)}")
+        except requests.exceptions.RequestException as err:
+            result[link] = SSiteStatus(available=False, code=400, info=f"Bad request: {str(err)}")
+        except Exception as err:
+            result[link] = SSiteStatus(available=False, code=1, info=f"Err: {str(err)}")
+    return SRequestModel(request=result)
+    
